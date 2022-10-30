21c8f1a0a5d91bc079ef6e31d95311270f7cb8b3
Oleg-Deda <106027183+Oleg-Deda@users.noreply.github.com>
2022-10-30 18:02:07 +0200

    finish first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
 
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img class="gallery__image" src=${preview} data-source=${original} alt=${description}/>
+  </a>
+</li>`
+    )
+    .join('');
+}
 
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+galleryEl.addEventListener('click', onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
 
+  const largeImage = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt=${event.target.alt}>`,
+    {
+      onShow: (largeImage) =>
+        galleryEl.addEventListener('keydown', onEscapeKeyPress),
+      onClose: (largeImage) =>
+        galleryEl.removeEventListener('keydown', onEscapeKeyPress),
+    }
+  );
+  largeImage.show();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(galleryItems);
+  function onEscapeKeyPress(event) {
+    if (event.key === 'Escape') {
+      largeImage.close();
+    }
+  }
+}
+galleryEl.addEventListener('click, onImageClick');
